673daedab4a8eb3660867b8de99d75c892a987b8
Laurent Bercot <ska-skaware@skarnet.org>
2020-11-29 19:40:51 +0000

    Adapt to new skalibs/exec.h

--- src/minutils/s6-chroot.c
+++ src/minutils/s6-chroot.c
@@ -1,20 +1,18 @@
 /* ISC license. */
 
-#ifndef _BSD_SOURCE
-#define _BSD_SOURCE
-#endif
-
+#include <skalibs/nonposix.h>
 #include <unistd.h>
+
 #include <skalibs/strerr2.h>
-#include <skalibs/djbunix.h>
+#include <skalibs/exec.h>
 
 #define USAGE "s6-chroot dir prog..."
 
-int main (int argc, char const *const *argv, char const *const *envp)
+int main (int argc, char const *const *argv)
 {
   PROG = "s6-chroot" ;
   if (argc < 3) strerr_dieusage(100, USAGE) ;
   if (chdir(argv[1]) == -1) strerr_diefu2sys(111, "chdir to ", argv[1]) ;
   if (chroot(".") == -1) strerr_diefu2sys(111, "chroot in ", argv[1]) ;
-  xpathexec_run(argv[2], argv+2, envp) ;
+  xexec(argv+2) ;
 }
--- src/minutils/s6-pivotchroot.c
+++ src/minutils/s6-pivotchroot.c
@@ -1,23 +1,21 @@
 /* ISC license. */
 
-#ifndef _BSD_SOURCE
-#define _BSD_SOURCE
-#endif
-
+#include <skalibs/nonposix.h>
 #include <unistd.h>
+
 #include <skalibs/strerr2.h>
-#include <skalibs/djbunix.h>
+#include <skalibs/exec.h>
 
 #define USAGE "s6-pivotchroot old-place-for-new-root new-place-for-old-root prog..."
 
 extern int pivot_root (char const *, char const *) ;
 
-int main (int argc, char const *const *argv, char const *const *envp)
+int main (int argc, char const *const *argv)
 {
   PROG = "s6-pivotchroot" ;
   if (argc < 4) strerr_dieusage(100, USAGE) ;
   if (chdir(argv[1]) < 0) strerr_diefu2sys(111, "chdir to ", argv[1]) ;
   if (pivot_root(".", argv[2]) < 0) strerr_diefu1sys(111, "pivot_root") ;
   if (chroot(".") < 0) strerr_diefu1sys(111, "chroot") ;
-  xpathexec_run(argv[3], argv+3, envp) ;
+  xexec(argv+3) ;
 }
