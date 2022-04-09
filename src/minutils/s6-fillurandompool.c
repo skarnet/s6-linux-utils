8add2dbec0842d5b051b4dba5536fba9d82ce6bc
Laurent Bercot <ska-skaware@skarnet.org>
2022-04-09 15:19:23 +0000

    Prepare for 2.5.1.8; adapt to skalibs-2.12.0.0
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/minutils/s6-fillurandompool.c
+++ src/minutils/s6-fillurandompool.c
@@ -1,14 +1,10 @@
 /* ISC license. */
 
-#include <skalibs/strerr2.h>
 #include <skalibs/random.h>
 
 int main (void)
 {
   char c ;
-  PROG = "s6-fillurandompool" ;
-  if (!random_init())
-    strerr_diefu1sys(111, "initialize random generator") ;
-  random_string(&c, 1) ;
+  random_buf(&c, 1) ;
   return 0 ;
 }
